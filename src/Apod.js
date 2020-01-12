a365a5e69768ef64e1ae2ef55684b29da5629dbe
tlewandowski18 <tlewandowski18@gmail.com>
2020-01-12 18:04:25 -0500

    add styling using styled components

--- src/Apod.js
+++ src/Apod.js
@@ -1,13 +1,36 @@
 import React from "react"
+import styled from "styled-components"
 
+const Desc = styled.p`
+    width: 75%;
+    height: 75%;
+    margin: 0 auto;
+    text-align: center;
+    padding: 2%;
+    font-size: 1.2rem
+`
+
+const SpaceImg = styled.img`    
+    width: 60%;
+    border-radius: 20px;
+`
+const Subheading = styled.h2`
+    font-size: 2.2rem;
+    color: ${props => props.photographer ? "orange" : "whitesmoke"};
+`
+
+const Heading = styled.h1`
+    font-size: 2.6rem;
+    color: blue;
+`
 function Apod(props) {
     return (
         <div>
-            <h1>{props.title}</h1>
-            <h2>{props.copyright}</h2>
-            <h2>{props.date}</h2>
-            <img src={props.url} />
-            <p>{props.explanation}</p>
+            <Heading>{props.title}</Heading>
+            <Subheading photographer>{props.copyright}</Subheading>
+            <Subheading>{props.date}</Subheading>
+            <SpaceImg src={props.url} />
+            <Desc>{props.explanation}</Desc>
         </div>
     )
 }
